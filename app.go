75cc839a6c6ae8a5022641a856800b6d758ef896
Alex Sinelnikov <sinelniko@gmail.com>
2015-05-30 23:15:05 +0300

    Initialize for router

--- app.go
+++ app.go
@@ -3,26 +3,20 @@ package goninja
 import (
 	"net/http"
 	"fmt"
-
 )
-
- var (
-	 router *Router
- )
 
 
 func Run() {
-
+	router := Router{}
+	router.New()
 	http.HandleFunc("/", handler)
 	http.ListenAndServe(":3000", nil)
 
-	router.New()
 
 
 }
 
 func handler(w http.ResponseWriter, r *http.Request){
-	fmt.Println("Initial start")
 	fmt.Println(w)
 	fmt.Println(r)
 }
